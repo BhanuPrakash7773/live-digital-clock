d8ab17e84ea7d7a5ed86cb57b1622919753c301a
BhanuPrakash7773 <160824229+BhanuPrakash7773@users.noreply.github.com>
2026-01-30 13:11:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function updateClock(){
   
   const now=new Date();
-  const hours=now.getHours().toString().padStart(2,0);
+  let hours=now.getHours().toString();
+  const meridiem=hours>=12 ? "PM" : "AM";
+  hours=hours % 12 // 12;
+  hours=hours.toString().padStart(2,0);
   const minutes=now.getMinutes().toString().padStart(2,0);
   const seconds=now.getSeconds().toString().padStart(2,0);
   const timeString=`${hours}:${minutes}:${seconds}`;
